65e1fef7383fe2e4dd474911df16201f70e75e50
Steve Dwire <steve@stevedwire.com>
2026-02-15 07:48:46 -0500

    Add GPLv3 copyright statement to previously missed file

--- src/main/tasks/ImportTmdbTask.ts
+++ src/main/tasks/ImportTmdbTask.ts
@@ -1,3 +1,11 @@
+/*
+Copyright (c) 2026 Steve Dwire
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3.
+*/
+
 import https from 'https';
 import fs from 'fs';
 import path from 'path';
