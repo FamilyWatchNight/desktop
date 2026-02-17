0058c75b68c0647566257793dff695c0f0dfeb4c
Steve Dwire <steve@stevedwire.com>
2026-02-16 21:47:02 -0500

    Remove obsolete (and false) comment

--- src/main/testing-noop/TestHooksImpl.ts
+++ src/main/testing-noop/TestHooksImpl.ts
@@ -1,5 +1,3 @@
-// The interface the application uses for these functions is provided in /src/main/testing/TestHooks.ts
-
 export interface TestHooks {};
 
 export function getTestHooks(): TestHooks {
