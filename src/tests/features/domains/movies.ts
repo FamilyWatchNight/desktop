a4da1d2179e1a1cd550ef82e83c327de65a2b29e
Steve Dwire <steve@stevedwire.com>
2026-03-06 16:25:12 -0600

    refactor: Clean up imports

--- src/tests/features/domains/movies.ts
+++ src/tests/features/domains/movies.ts
@@ -6,8 +6,8 @@ it under the terms of the GNU General Public License as published by
 the Free Software Foundation, version 3.
 */
 
-import { ElectronApplication, Page } from 'playwright';
-import { MovieData } from '../../../../main/db/models/Movies';
+import { ElectronApplication } from 'playwright';
+import { MovieData } from '../../../main/db/models/Movies';
 import { withTestHooks } from '../infrastructure/utils';
 
 /**
