79c25c9dec9c5f44c89fbf40a4605e1412f3d42b
Steve Dwire <steve@stevedwire.com>
2026-03-20 13:17:32 -0400

    fix: export appLanguage so that the API calls can return it.

--- src/main/i18n.ts
+++ src/main/i18n.ts
@@ -6,6 +6,8 @@ import { app } from 'electron';
 const isDevMode = !app.isPackaged;
 const i18nPath = path.join(app.getAppPath(), 'assets/locales');
 
+export const appLanguage = ( process.env.NODE_ENV === 'test' ) ? 'test' : isDevMode ? 'dev' : app.getLocale();
+
 i18n
   .use(Backend)
   .init({
@@ -19,7 +21,7 @@ i18n
     fallbackLng: ( process.env.NODE_ENV === 'test' ) ? 'test' : isDevMode ? 'dev' : 'en',
     initImmediate: false, 
     interpolation: { escapeValue: false},
-    lng: ( process.env.NODE_ENV === 'test' ) ? 'test' : isDevMode ? 'dev' : app.getLocale(),
+    lng: appLanguage,
     load: 'all',
     ns: ['main', 'common'],
     parseMissingKeyHandler: (key) => `[!!!${key}!!!]`,
